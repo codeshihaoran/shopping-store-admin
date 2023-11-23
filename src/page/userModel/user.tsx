abd8107b9d5906724450f5a3e957ac38173a24cb
shihaoran <shihaoran.code@gmail.com>
2023-11-24 00:24:41 +0800

    style/user

--- src/page/userModel/user.tsx
+++ src/page/userModel/user.tsx
@@ -4,7 +4,26 @@ const User = () => {
     return (
         <div className="user">
             <div className="view-main">
-                我是用户界面
+                <div className="user-container">
+                    <div className="user-table-header">
+                        <div className="table-cell">User ID</div>
+                        <div className="table-cell">User Name</div>
+                        <div className="table-cell">Phone Number</div>
+                        <div className="table-cell">Address</div>
+                        <div className="table-cell">Actions</div>
+                    </div>
+                    <ul className="user-list">
+                        <li className="user-item">
+                            <div className="user-value">xxxx</div>
+                            <div className="user-value">xxxx</div>
+                            <div className="user-value">1234567890</div>
+                            <div className="user-value">xxxx</div>
+                            <div className="user-value">
+                                <button className="view-orders-btn">View Orders</button>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
