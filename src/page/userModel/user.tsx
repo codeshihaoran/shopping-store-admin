9cd448a7687de5ebcb0ba29e6253b00b4dfa950e
shihaoran <shihaoran.code@gmail.com>
2023-11-24 01:43:26 +0800

    feat: user/view

--- src/page/userModel/user.tsx
+++ src/page/userModel/user.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './user.less'
+import axios from "axios";
+import { UserInfo } from "@/type/user";
+import { useNavigate } from "react-router-dom";
 const User = () => {
+    const navigate = useNavigate()
+    const [allUserInfo, setAllUserInfo] = useState([])
+    useEffect(() => {
+        axios.post('/api/users/allUserInfo/get').then(res => {
+            if (res.data.code === '001') {
+                setAllUserInfo(res.data.allUserInfo)
+            }
+        }).catch(err => {
+            console.log(err);
+        })
+    }, [])
     return (
         <div className="user">
             <div className="view-main">
@@ -12,16 +26,17 @@ const User = () => {
                         <div className="table-cell">Address</div>
                         <div className="table-cell">Actions</div>
                     </div>
+                    {/* 个人地址后续去做 需要配合store 填写地址后获取 */}
                     <ul className="user-list">
-                        <li className="user-item">
-                            <div className="user-value">xxxx</div>
-                            <div className="user-value">xxxx</div>
-                            <div className="user-value">1234567890</div>
-                            <div className="user-value">xxxx</div>
+                        {allUserInfo.map((item: UserInfo) => <li key={item.user_id} className="user-item">
+                            <div className="user-value">{item.user_id}</div>
+                            <div className="user-value">{item.user_name}</div>
+                            <div className="user-value">{item.user_phone}</div>
+                            <div className="user-value">奥特曼帝国</div>
                             <div className="user-value">
-                                <button className="view-orders-btn">View Orders</button>
+                                <button className="view-orders-btn" onClick={() => navigate(`/order?userId=${item.user_id}`)}>个人订单</button>
                             </div>
-                        </li>
+                        </li>)}
                     </ul>
                 </div>
             </div>
---
+++ src/type/user.ts
@@ -0,0 +1,6 @@
+export interface UserInfo {
+    user_id: number
+    user_name: string
+    user_password: string
+    user_phone: number
+}
