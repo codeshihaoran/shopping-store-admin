105b5fe8d8b311c8a5384d9fe814e46434f3c392
shihaoran <shihaoran.code@gmail.com>
2023-12-24 22:01:12 +0800

    feat: home/Table

--- src/page/homeModel/home.tsx
+++ src/page/homeModel/home.tsx
@@ -1,18 +1,66 @@
 import React, { useEffect, useState } from "react";
 import './home.less'
-import { Button, Col, Row, Statistic, Card } from 'antd';
+import { Col, Row, Card, Space, Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 import axios from "axios";
+interface DataType {
+    key: string;
+    name: string;
+    phone: number;
+    address: string;
+    tag: string;
+}
+const columns: ColumnsType<DataType> = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text) => <a>{text}</a>,
+    },
+    {
+        title: 'Phone',
+        dataIndex: 'phone',
+        key: 'phone',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+    },
+    {
+        title: 'Tag',
+        key: 'tag',
+        dataIndex: 'tag',
+        render: (text) => <Tag color={"green"} style={{ fontWeight: '400', fontSize: '14px', height: '25px', lineHeight: '25px' }}>
+            {text}
+        </Tag>
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: () => (
+            <Space size="middle">
+                <a>删除订单</a>
+                <a>提醒支付</a>
+            </Space>
+        ),
+    },
+]
 const Home = () => {
-    const [orderSale, OrderSale] = useState({
+    const [orderSale, SetOrderSale] = useState({
         salePrice: 0,
         saleSum: 0,
         userSum: 0
     })
+    const [data, setData] = useState([])
     useEffect(() => {
         axios.post('/api/admin/order/sale/get').then(res => {
             if (res.data.code === '001') {
                 const { userSum, saleSum, salePrice } = res.data;
-                OrderSale({ userSum, saleSum, salePrice })
+                const List = res.data.pendingList
+                SetOrderSale({ userSum, saleSum, salePrice })
+                setData(List)
+                console.log(data);
             }
         }).catch(err => {
             console.log(err);
@@ -23,7 +71,7 @@ const Home = () => {
             <div className="view-main">
                 <div className="dash-board">
                     <span>订单数据</span>
-                    <Row gutter={16} style={{ marginTop: '20px' }}>
+                    <Row gutter={16} style={{ margin: '20px 0px' }}>
                         <Col span={8}>
                             <Card title="订单数量" bordered={false} style={{ border: '1px solid lightblue', paddingRight: '20px' }} >
                                 {orderSale.saleSum}
@@ -40,6 +88,18 @@ const Home = () => {
                             </Card>
                         </Col>
                     </Row>
+                    <span>待办事务</span>
+                    <Table columns={columns} dataSource={data} style={{ margin: '20px 0px' }}
+                        pagination={{
+                            itemRender: (page, type, originalElement) => {
+                                return (
+                                    <div style={{ marginTop: '-8px' }}>
+                                        {originalElement}
+                                    </div>
+                                )
+                            }
+                        }}
+                    />
                 </div>
             </div>
         </div >
